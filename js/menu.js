9d039c94b1dd6d1d945c55ecf29c780a5661d2d5
Matias Viaggio <matiasviaggio59@gmail.com>
2022-09-11 20:17:58 -0300

    carrito javascript

---
+++ js/menu.js
@@ -0,0 +1,465 @@
+let entradas = [
+
+    {id: 0,
+        
+    nombre: 'entrada fria',
+
+    img: '../assets/menu/1entradas/entradaFria.jpg',
+
+    descripcion: 'Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien fringilla, mattis ligula consectetur, ultrices. Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien fringilla, mattis ligula consectetur, ultrices.', 
+
+    fire: '../assets/icons/menu/fire.jpg', 
+
+    precio: 4200, 
+
+    oveg: 'SI', 
+
+    condimentos: 'Condimentos: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut suscipit eu odio nec gravida. Vestibulum vel cursus enim.',},
+
+    {id: 1,
+        
+    nombre: 'corn deluxe',
+
+    img: '../assets/menu/1entradas/cornDeluxe.jpg',
+
+    descripcion: 'Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien fringilla, mattis ligula consectetur, ultrices.', 
+
+    fire: '../assets/icons/menu/fire.jpg', 
+
+    precio: 2100, 
+
+    oveg: 'SI', 
+    
+    condimentos: 'Condimentos: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut suscipit eu odio nec gravida. Vestibulum vel cursus enim.',
+
+    orientacion: 'order-lg-1',
+
+    },
+
+    {id: 2,
+        
+    nombre: 'rabas',
+
+    img: '../assets/menu/1entradas/rabas.jpg',
+
+    descripcion: 'Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien fringilla, mattis ligula consectetur, ultrices.', 
+
+    fire: '../assets/icons/menu/fire.jpg', 
+
+    precio: 3400, 
+
+    oveg: 'NO', 
+    
+    condimentos: 'Condimentos: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut suscipit eu odio nec gravida. Vestibulum vel cursus enim.',
+
+    orientacion: '',
+
+    },
+
+    {id: 3,
+        
+    nombre: 'tabla de quesos',
+
+    img: '../assets/menu/1entradas/tablaDeQuesos.jpg',
+
+    descripcion: 'Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien fringilla, mattis ligula consectetur, ultrices.', 
+
+    fire: '../assets/icons/menu/fire.jpg', 
+
+    precio: 3600, 
+
+    oveg: 'NO', 
+    
+    condimentos: 'Condimentos: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut suscipit eu odio nec gravida. Vestibulum vel cursus enim.', 
+    
+    orientacion: 'order-lg-1',
+
+    },
+
+]
+
+let ensaladas = [
+    {id: 0,
+        
+        nombre: 'premium salad',
+    
+        img: '../assets/menu/2ensaladas/ensaladaPremium.jpg',
+    
+        descripcion: 'Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien fringilla, mattis ligula consectetur, ultrices. Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien fringilla, mattis ligula consectetur, ultrices.', 
+    
+        fire: '../assets/icons/menu/fire.jpg', 
+    
+        precio: ' $ $ $ ', 
+    
+        oveg: 'SI', 
+    
+        condimentos: 'Condimentos: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut suscipit eu odio nec gravida. Vestibulum vel cursus enim.',},
+    
+        {id: 1,
+            
+        nombre: 'fresh salad',
+    
+        img: '../assets/menu/2ensaladas/freshSalad.jpg',
+    
+        descripcion: 'Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien fringilla, mattis ligula consectetur, ultrices.', 
+    
+        fire: '../assets/icons/menu/fire.jpg', 
+    
+        precio: ' $ $ ', 
+    
+        oveg: 'SI', 
+        
+        condimentos: 'Condimentos: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut suscipit eu odio nec gravida. Vestibulum vel cursus enim.',
+    
+        orientacion: 'order-lg-1',
+    
+        },
+]
+
+let carnes = [
+    
+    {id: 0,
+        
+        nombre: 'tomahawk',
+        
+        img: '../assets/menu/3platosPrincipales/carnesRojas/tomaHawk.jpg',
+        
+        descripcion: 'Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien fringilla, mattis ligula consectetur, ultrices.', 
+        
+        fire: '../assets/icons/menu/fire.jpg', 
+        
+        precio: ' $ $ $ $', 
+        
+        oveg: 'NO', 
+        
+        condimentos: 'Condimentos: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut suscipit eu odio nec gravida. Vestibulum vel cursus enim.', 
+        
+        orientacion: '',
+        
+    },
+    
+    {id: 1,
+        
+    nombre: 'costillar de cerdo',
+
+    img: '../assets/menu/3platosPrincipales/carnesRojas/pechitoDeCerdo.jpg',
+
+    descripcion: 'Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien fringilla, mattis ligula consectetur, ultrices.', 
+
+    fire: '../assets/icons/menu/fire.jpg', 
+
+    precio: ' $ $ $ $', 
+
+    oveg: 'NO', 
+    
+    condimentos: 'Condimentos: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut suscipit eu odio nec gravida. Vestibulum vel cursus enim.', 
+    
+    orientacion: 'order-lg-1',
+
+    },
+
+    {id: 2,
+        
+    nombre: 'entraña con papas',
+
+    img: '../assets/menu/3platosPrincipales/carnesRojas/carneConPapas.jpg',
+
+    descripcion: 'Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien fringilla, mattis ligula consectetur, ultrices.', 
+
+    fire: '../assets/icons/menu/fire.jpg', 
+
+    precio: ' $ $ $ $ $ ', 
+
+    oveg: 'NO', 
+    
+    condimentos: 'Condimentos: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut suscipit eu odio nec gravida. Vestibulum vel cursus enim.',
+
+    orientacion: '',
+    
+    },
+
+    {id: 3,
+        
+    nombre: 'pechito de cerdo',
+
+    img: '../assets/menu/3platosPrincipales/carnesRojas/carneConEsparragos.jpg',
+
+    descripcion: 'Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien fringilla, mattis ligula consectetur, ultrices.', 
+
+    fire: '../assets/icons/menu/fire.jpg', 
+
+    precio: ' $ $ $ ', 
+
+    oveg: 'NO', 
+    
+    condimentos: 'Condimentos: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut suscipit eu odio nec gravida. Vestibulum vel cursus enim.',
+
+    orientacion: 'order-lg-1',
+
+    },
+
+    {id: 4,
+        
+    nombre: 'brochetas de carne',
+
+    img: '../assets/menu/3platosPrincipales/carnesRojas/brochetasDeCarne.jpg',
+
+    descripcion: 'Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien fringilla, mattis ligula consectetur, ultrices. Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien fringilla, mattis ligula consectetur, ultrices.', 
+
+    fire: '../assets/icons/menu/fire.jpg', 
+
+    precio: ' $ $ $ $ ', 
+
+    oveg: 'NO', 
+
+    condimentos: 'Condimentos: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut suscipit eu odio nec gravida. Vestibulum vel cursus enim.',
+    
+    orientacion: '',
+    
+    },
+]
+
+
+// CONSTRUCTOR DE TIENDA
+
+
+const buildEntradas = () => {
+    let contenedor = document.getElementById("container-entradas");
+    entradas.forEach((entrada, index) => {
+        let card = document.createElement("div");
+        card.classList.add("row", "mi-row");
+        card.innerHTML = `
+        <div class="col-lg-7 ${entrada.orientacion} col-md-12 gx-0 mi-img-container">
+        <img loading="lazy" src="${entrada.img}" alt="${entrada.nombre}">
+        </div>
+        <div class="col-lg-5 col-md-12 gx-0 mi-desc-container">
+            <div class="mi-desc">
+                <div class="text-center mi-desc-title">
+                    <h4>
+                        ${entrada.nombre}
+                    </h4>
+                </div>
+                <div class="text-center mi-desc-p">
+                    <p>
+                        ${entrada.descripcion}
+                    </p>
+                </div>
+                <div class="mi-desc-details">
+                    <div class="container-fluid">
+                        <div class="row details-row">
+                            <div class="col-lg-6 col-md-12 details-items">
+                                <div class="detail-calific">
+                                    <h5>Calificacion:</h5>
+                                    <div class="detail-calific-img">
+                                        
+                                    </div>       
+                                </div>
+                                <h5>Precio: ${entrada.precio}</h5>
+                                <h5>Opcion vegetariana: ${entrada.oveg}</h5>
+                                <button class='btn aa-carrito-btn' onClick="addToCart(${index})">Añadir al carrito</button><br>
+                                <a href="#indice">Ir al indice</a>
+                            </div>
+                            <div class="col-lg-6 col-md-12 details-ing-container">
+                                <h6 class="text-center">lista completa de ingredientes</h6>
+                                <ul>
+                                    <li>Ingrediente #1</li>
+                                    <li>Ingrediente #2</li>
+                                    <li>Ingrediente #3</li>
+                                    <li>Ingrediente #4</li>
+                                </ul>
+                                <p>
+                                    ${entrada.condimentos}
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        `;
+        contenedor.appendChild(card)
+    })
+};
+
+const buildEnsaladas = () => {
+    let contenedor = document.getElementById("container-ensaladas");
+    ensaladas.forEach((ensalada, index) => {
+        let card = document.createElement("div");
+        card.classList.add("row", "mi-row");
+        card.innerHTML = `
+        <div class="col-lg-7 ${ensalada.orientacion} col-md-12 gx-0 mi-img-container">
+        <img loading="lazy" src="${ensalada.img}" alt="${ensalada.nombre}">
+        </div>
+        <div class="col-lg-5 col-md-12 gx-0 mi-desc-container">
+            <div class="mi-desc">
+                <div class="text-center mi-desc-title">
+                    <h4>
+                        ${ensalada.nombre}
+                    </h4>
+                </div>
+                <div class="text-center mi-desc-p">
+                    <p>
+                        ${ensalada.descripcion}
+                    </p>
+                </div>
+                <div class="mi-desc-details">
+                    <div class="container-fluid">
+                        <div class="row details-row">
+                            <div class="col-lg-6 col-md-12 details-items">
+                                <div class="detail-calific">
+                                    <h5>Calificacion:</h5>
+                                    <div class="detail-calific-img">
+                                        
+                                    </div>       
+                                </div>
+                                <h5>Precio: ${ensalada.precio}</h5>
+                                <h5>Opcion vegetariana: ${ensalada.oveg}</h5>
+                                <a href="#" class="btn aa-carrito-btn" onClick="addToCart(${index})">Añadir al carrito</a><br>
+                                <a href="#indice">Ir al indice</a>
+                            </div>
+                            <div class="col-lg-6 col-md-12 details-ing-container">
+                                <h6 class="text-center">lista completa de ingredientes</h6>
+                                <ul>
+                                    <li>Ingrediente #1</li>
+                                    <li>Ingrediente #2</li>
+                                    <li>Ingrediente #3</li>
+                                    <li>Ingrediente #4</li>
+                                </ul>
+                                <p>
+                                    ${ensalada.condimentos}
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        `;
+        contenedor.appendChild(card)
+    })
+};
+
+const buildCarnes = () => {
+    let contenedor = document.getElementById("container-carnes");
+    carnes.forEach((carne, index) => {
+        let card = document.createElement("div");
+        card.classList.add("row", "mi-row");
+        card.innerHTML = `
+        <div class="col-lg-7 ${carne.orientacion} col-md-12 gx-0 mi-img-container">
+        <img loading="lazy" src="${carne.img}" alt="${carne.nombre}">
+        </div>
+        <div class="col-lg-5 col-md-12 gx-0 mi-desc-container">
+            <div class="mi-desc">
+                <div class="text-center mi-desc-title">
+                    <h4>
+                        ${carne.nombre}
+                    </h4>
+                </div>
+                <div class="text-center mi-desc-p">
+                    <p>
+                        ${carne.descripcion}
+                    </p>
+                </div>
+                <div class="mi-desc-details">
+                    <div class="container-fluid">
+                        <div class="row details-row">
+                            <div class="col-lg-6 col-md-12 details-items">
+                                <div class="detail-calific">
+                                    <h5>Calificacion:</h5>
+                                    <div class="detail-calific-img">
+                                        
+                                    </div>       
+                                </div>
+                                <h5>Precio: ${carne.precio}</h5>
+                                <h5>Opcion vegetariana: ${carne.oveg}</h5>
+                                <button class='btn aa-carrito-btn' onClick="addToCart(${index})">Añadir al carrito</button><br>
+                                <a href="#indice">Ir al indice</a>
+                            </div>
+                            <div class="col-lg-6 col-md-12 details-ing-container">
+                                <h6 class="text-center">lista completa de ingredientes</h6>
+                                <ul>
+                                    <li>Ingrediente #1</li>
+                                    <li>Ingrediente #2</li>
+                                    <li>Ingrediente #3</li>
+                                    <li>Ingrediente #4</li>
+                                </ul>
+                                <p>
+                                    ${carne.condimentos}
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        `;
+        contenedor.appendChild(card)
+    })
+};
+
+buildEntradas();
+buildEnsaladas();
+buildCarnes();
+
+// CARRITO INICIAL
+
+let cart = [];
+
+// TOTAL INICIAL DEL CARRITO
+
+let total = 0;
+
+// MODAL CARRITO
+
+let modalCarrito = document.getElementById('cart-container');
+
+// CARRITO CHECKER
+
+const addToCart = (index) => {
+    
+    const indexFinder = cart.findIndex((item) => {
+        return item.id === entradas[index].id;
+    });
+    if (indexFinder === -1) {
+        const addItem = entradas[index]
+        addItem.quant = 1;
+        cart.push(addItem);
+        alert('Producto añadido con éxito!')
+        buildEntradasCart();
+    }else {
+        cart[indexFinder].quant += 1;
+        alert('Producto añadido con éxito!')
+        buildEntradasCart();
+    }
+};
+
+
+// CONTRUCTOR DE CARRITO
+
+const buildEntradasCart = () => {
+    modalCarrito.innerHTML = '';
+    if (cart.length > 0) {
+        cart.forEach ((entrada, index) => {
+            total = total + entrada.precio * entrada.quant;
+            const carritoContainer = document.createElement('div');
+            carritoContainer.className = 'cart-row';
+            carritoContainer.innerHTML = `
+            <div class="col-2 cart-img">
+                <img src="${entrada.img}" alt="${entrada.nombre}">
+                </div>
+                <div class="col-2 cart-name"><p>${entrada.nombre}</p></div>
+                <div class="col-2 cart-price"><p>$${entrada.precio}</p></div>
+                <div class="col-2 cart-quant"><p>(${entrada.quant})</p></div>
+                <div class="col-2 cart-delete"><p><button class="btn aa-carrito-btn" onClick="removeProduct(${index})">eliminar</button></p></div>
+                <div class="col-2 cart-price">$${entrada.precio * entrada.quant}</div>
+            </div> 
+            `;
+            modalCarrito.appendChild(carritoContainer);
+        })
+    }
+};
+
+const removeProduct = (index) => {
+    cart.splice(index,1);
+    buildEntradasCart();
+}
